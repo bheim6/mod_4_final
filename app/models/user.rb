d3f1825010c5e440319864369900b4cec71086da
Brian Heim <bheim67@gmail.com>
2017-01-09 11:58:04 -0700

    add pass confirm to user params, add validations for incorrect fields, blank fields, and test

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@ class User < ApplicationRecord
   has_many :links
 
   validates :password_digest, presence: true
+  validates :password, presence: true
+  validates :password, confirmation: true
+  validates :password_confirmation, presence: true
+
   validates :email, presence: true
   validates :email, uniqueness: true
 end
--- spec/features/user_can_sign_up_and_login_spec.rb
+++ spec/features/user_can_sign_up_and_login_spec.rb
@@ -17,9 +17,49 @@ RSpec.feature "Users can sign up and login" do
     expect(page).to have_content('Account Created!')
   end
 
-  # I cannot sign up with an email address that has already been used.
-  # Password and confirmation must match.
+  scenario "user cannot sign up with an email address that has already been used" do
+    user1 = create(:user)
+    visit '/'
+    # I should be redirected to a page which prompts me to "Log In or Sign Up".
+    expect(current_path).to eq('/signup')
+
+    fill_in "Email", with: "example@example.com"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password"
+    click_on "Submit"
+    # Upon submitting this information, I should be logged in via a session cookie and redirected to the "Links Index" page.
+    expect(page).to have_content('Email has already been taken')
+  end
+
+# Password and confirmation must match.
+  scenario "Password and confirmation must match" do
+    visit '/'
+    # I should be redirected to a page which prompts me to "Log In or Sign Up".
+    expect(current_path).to eq('/signup')
+
+    fill_in "Email", with: "example@example.com"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "pas"
+    click_on "Submit"
+    # Upon submitting this information, I should be logged in via a session cookie and redirected to the "Links Index" page.
+    expect(page).to have_content("Password confirmation doesn't match Password")
+  end
   # If criteria is not met the user should be given a message to reflect the reason they could not sign up.
+
+  scenario "fields cannot be blank" do
+    visit '/'
+    # I should be redirected to a page which prompts me to "Log In or Sign Up".
+    expect(current_path).to eq('/signup')
+
+    fill_in "Email", with: ""
+    fill_in "Password", with: ""
+    fill_in "Password confirmation", with: ""
+    click_on "Submit"
+    # Upon submitting this information, I should be logged in via a session cookie and redirected to the "Links Index" page.
+    expect(page).to have_content("Email can't be blank")
+    expect(page).to have_content("Password can't be blank")
+    expect(page).to have_content("Password confirmation can't be blank")
+  end
 
   scenario "authenticated users can log in and then sign out" do
     user1 = create(:user)
