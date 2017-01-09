677d60e7f4132297f4dcdee6c0c0072a5acab798
Brian Heim <bheim67@gmail.com>
2017-01-09 10:33:27 -0700

    add validations and associaions for user and test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
+  has_many :links
+
+  validates :password_digest, presence: true
+  validates :email, presence: true
   validates :email, uniqueness: true
 end
